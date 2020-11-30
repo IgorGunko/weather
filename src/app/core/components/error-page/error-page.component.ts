b8b25c9f361522c9eb6062ce88e7c405139fe412
IgorGunko <igorgunko0@gmail.com>
2020-11-30 23:23:14 +0200

    Add changeDetection onPush

--- src/app/core/components/error-page/error-page.component.ts
+++ src/app/core/components/error-page/error-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.scss']
+  styleUrls: ['./error-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorPageComponent implements OnInit {
   public statusCode: number;
